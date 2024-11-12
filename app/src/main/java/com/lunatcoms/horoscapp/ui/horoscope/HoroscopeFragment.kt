bf7c00c2c34ba342f60737a92f5ec33c3aee6551
leolunaf25 <lunalunaf25@gmail.com>
2024-11-11 20:52:20 -0600

    add transition animations

--- app/src/main/java/com/lunatcoms/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/lunatcoms/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.lunatcoms.horoscapp.databinding.FragmentHoroscopeBinding
 import com.lunatcoms.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
@@ -38,6 +39,9 @@ class HoroscopeFragment : Fragment() {
     private fun initList() {
         horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
             Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show()
+            findNavController().navigate(
+                HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity()
+            )
         })
 
         binding.rvHoroscope.apply {
