e46541465520d8e160704c0fd16c2b00486814b3
leolunaf25 <lunalunaf25@gmail.com>
2024-12-09 22:58:30 -0600

    add UI horoscope detail

--- app/src/main/java/com/lunatcoms/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/lunatcoms/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,10 +3,16 @@ package com.lunatcoms.horoscapp.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.lunatcoms.horoscapp.R
 import com.lunatcoms.horoscapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -20,8 +26,35 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
 
-        args.type
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeDetailViewModel.state.collect{
+                    when(it){
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+
+    }
+    private fun errorState() {
+
+    }
+    private fun successState() {
 
     }
 }
